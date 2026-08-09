e6d42c9b715e57068712b550af0c8622cdc608b2
Astrophotography With Yash <yashraj01822@gmail.com>
2026-08-09 13:05:50 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-
-  import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  plugins: [react()],
-  base: "/cosmo-vision/"
-});
+  base: "/cosmo-vision/",
 });
