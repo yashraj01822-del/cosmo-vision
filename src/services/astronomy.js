1aba0abb4dc68a59d93ca2e3f8f019bf06b0e250
Astrophotography With Yash <yashraj01822@gmail.com>
2026-08-09 08:36:06 +0530

    Update astronomy.js

--- src/services/astronomy.js
+++ src/services/astronomy.js
@@ -1,1 +1,110 @@
+import * as Astronomy from "astronomy-engine";
 
+const PLANETS = [
+  "Mercury",
+  "Venus",
+  "Mars",
+  "Jupiter",
+  "Saturn"
+];
+
+export function getSkyObjects(
+  latitude,
+  longitude,
+  date = new Date()
+) {
+  const observer = new Astronomy.Observer(
+    latitude,
+    longitude,
+    0
+  );
+
+  const objects = [];
+
+  // Sun
+  objects.push(
+    getBodyPosition(
+      "Sun",
+      Astronomy.Body.Sun,
+      observer,
+      date
+    )
+  );
+
+  // Moon
+  objects.push(
+    getBodyPosition(
+      "Moon",
+      Astronomy.Body.Moon,
+      observer,
+      date
+    )
+  );
+
+  // Planets
+  for (const name of PLANETS) {
+    objects.push(
+      getBodyPosition(
+        name,
+        Astronomy.Body[name],
+        observer,
+        date
+      )
+    );
+  }
+
+  return objects;
+}
+
+
+function getBodyPosition(
+  name,
+  body,
+  observer,
+  date
+) {
+  const equator =
+    Astronomy.Equator(
+      body,
+      date,
+      observer,
+      true,
+      true
+    );
+
+  const horizontal =
+    Astronomy.Horizon(
+      date,
+      observer,
+      equator.ra,
+      equator.dec,
+      "normal"
+    );
+
+  return {
+    name,
+
+    azimuth:
+      normalizeAngle(horizontal.azimuth),
+
+    altitude:
+      horizontal.altitude,
+
+    ra:
+      equator.ra,
+
+    dec:
+      equator.dec,
+
+    visible:
+      horizontal.altitude > 0
+  };
+}
+
+
+function normalizeAngle(angle) {
+  return (
+    (angle % 360) +
+    360
+  ) % 360;
+}
